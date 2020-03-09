7ae1f3713aff36291883434a42946f4396e931c8
Carlos Diaz <carlosdiaz723@yahoo.com>
2020-03-09 16:35:01 -0400

    comments on four

--- four/mainProgram.py
+++ four/mainProgram.py
@@ -1,19 +1,41 @@
 '''
-Carlos Diaz
-Module 4 -- Assignment 3
-'''
+Names / Email:
+Carlos Diaz             cdiaz29@students.kennesaw.edu
 
+College of Computing and Software Engineering
+Department of Computer Science
+---------------------------------------------
+CS4308: CONCEPTS OF PROGRAMMING LANGUAGES
+SECTION W01 – SPRING 2020
+---------------------------------------------
+Module 4
+Assignment 3
+
+
+File Description:
+This file runs the main loop that adds creates students, adds them to a list,
+and prints them out.
+'''
+# importing the Student class
 from student import Student
 
+# create the array of students
 arrayOfStudents = list()
 
 
 def addStudent():
+    '''
+    method definition for prompting information and adding a student
+    '''
+    # data gathering
     name = input("Enter student name: ")
     stuID = input("Enter " + name + "'s student ID: ")
     numCourses = input("Enter number of courses for " + name + ": ")
+
+    # create student object
     myStudent = Student(name, stuID, numCourses)
 
+    # change one attribute using setters
     print("\nAdding 1 to the number of courses for ",
           myStudent.getName(), ".", sep='')
     print("Previous number of courses: ",
@@ -22,26 +44,38 @@ def addStudent():
     print("Number of courses after adding 1: ",
           myStudent.getNumCourses(), sep='')
 
+    # add student into array
     print("\nAdding ", myStudent.getName(), " into the array.", sep='')
     arrayOfStudents.append(myStudent)
+
+    # print array of all students
     print("Updated list of students: ", sep='')
     printAllStudents()
 
 
 def printAllStudents():
+    '''
+    method definition for printing student
+    array in a visually consistent manner
+    '''
     stringToPrint = '{'
     for student in arrayOfStudents:
         stringToPrint += student.toString() + ',\n'
     print(stringToPrint[:-2] + "}")
 
 
+# start of main code to be ran on script call
+# start with adding at least one student
 addStudent()
+
+# loop through prompts until no more students left to input
 while True:
     answer = input("Add another student? (Y/N)")
     print('\n\n')
     if answer.lower() in ['y', 'yes']:
         addStudent()
     else:
+        # once user prompts not to continue, print all students and break loop
         print("Final status of array: ")
         printAllStudents()
         break
--- four/student.py
+++ four/student.py
@@ -1,6 +1,31 @@
-class Student():
+'''
+Names / Email:
+Carlos Diaz             cdiaz29@students.kennesaw.edu
 
+College of Computing and Software Engineering
+Department of Computer Science
+---------------------------------------------
+CS4308: CONCEPTS OF PROGRAMMING LANGUAGES
+SECTION W01 – SPRING 2020
+---------------------------------------------
+Module 4
+Assignment 3
+
+
+File Description:
+This file contains the Student class which has getters, setters, and a toString
+function that allows for easy printing of student information.
+'''
+
+
+class Student():
+    '''
+    Student class definition
+    '''
+
+    # constructor
     def __init__(self, name: str, stuID, numCourses: int):
+        # ensure numCourses is an integer
         try:
             numCourses = int(numCourses)
         except TypeError:
@@ -9,6 +34,7 @@ class Student():
         self.stuID = stuID
         self.numCourses = numCourses
 
+    # setters
     def setName(self, newName):
         self.name = newName
 
@@ -18,6 +44,7 @@ class Student():
     def setNumCourses(self, numCourses):
         self.numCourses = numCourses
 
+    # getters
     def getName(self):
         return self.name
 
@@ -27,6 +54,7 @@ class Student():
     def getNumCourses(self):
         return self.numCourses
 
+    # toString
     def toString(self):
         return str(self.name + ", ID: " + self.stuID +
                    ", Number of courses: " + str(self.numCourses))
